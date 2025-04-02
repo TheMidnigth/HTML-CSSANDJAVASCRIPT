b97690a0050224fdb9e6d54c8fc2986c260e7e39
TheMidnight <reyeskeyner110@gmail.com>
2025-04-01 23:16:30 -0500

    termine el html y css del registro solo falta ahora el login y el javascript y listo

--- ejerciciohtml,css and javascript/Buques De Cargas/js/formularios.js
+++ ejerciciohtml,css and javascript/Buques De Cargas/js/formularios.js
@@ -29,35 +29,5 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 
-    // Selección de elementos de la contraseña y los íconos
-    const passwordInput = document.getElementById("password");
-    const toggleIcon = document.getElementById("toggleIcon");
-    const passwordInput2 = document.getElementById("repeatPassword");
-    const toggleIcon2 = document.getElementById("toggleIcon2");
-
-    // Alternar visibilidad de la contraseña en el primer input
-    if (passwordInput && toggleIcon) {
-        toggleIcon.addEventListener("click", function () {
-            if (passwordInput.type === "password") {
-                passwordInput.type = "text";
-                toggleIcon.classList.replace("fa-eye", "fa-eye-slash");
-            } else {
-                passwordInput.type = "password";
-                toggleIcon.classList.replace("fa-eye-slash", "fa-eye");
-            }
-        });
-    }
-
-    // Alternar visibilidad de la contraseña en el segundo input
-    if (passwordInput2 && toggleIcon2) {
-        toggleIcon2.addEventListener("click", function () {
-            if (passwordInput2.type === "password") {
-                passwordInput2.type = "text";
-                toggleIcon2.classList.replace("fa-eye", "fa-eye-slash");
-            } else {
-                passwordInput2.type = "password";
-                toggleIcon2.classList.replace("fa-eye-slash", "fa-eye");
-            }
-        });
-    }
+    
 });
