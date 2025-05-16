18933051e30d50e55135dedbde9db604e822fe0a
TheMidnight <reyeskeyner110@gmail.com>
2025-05-16 17:45:03 -0500

    calendario antiguo reemplazado por uno nuevo

--- ejerciciohtml,css and javascript/Buques De Cargas/js/scripts/hamburger.js
+++
@@ -1,9 +0,0 @@
-export function initHamburger() {
-  const hamburgetButtonElement = document.querySelector("[data-hamburger-button]");
-
-  hamburgetButtonElement.addEventListener("click", () => {
-    hamburgetButtonElement.dispatchEvent(new CustomEvent("mobile-sidebar-open-request", {
-      bubbles: true
-    }));
-  });
-}
